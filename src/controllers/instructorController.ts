cf9bcd60d349dd72e58647a2c4c35af057cf6d91
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-08-22 18:54:49 -0300

    fix: fix in parts register controller

--- src/controllers/instructorController.ts
+++ src/controllers/instructorController.ts
@@ -2,27 +2,26 @@ import { StatusCodes } from "http-status-codes";
 import Instructor from "../models/instructorModel";
 import { Request, Response } from "express";
 import jwt from 'jsonwebtoken'
+import customApiErrors from "../errors/customApiErrors";
 
-const JWTSECRET = process.env.JWT_SECRET
 
-export const register = async (req: Request, res: Response)=>{
-    try {
-        if(!JWTSECRET){
-            res.sendStatus(StatusCodes.UNPROCESSABLE_ENTITY)
-            return
-        }
-        const instructor = await Instructor.create({...req.body})
-        const token = await jwt.sign({email: instructor.email, name: instructor.name}, JWTSECRET)
-        res.status(StatusCodes.CREATED).json({instructor: {name: instructor.name}, token})
-    } catch (error) {
-        res.send(error)
+export const register = async (req: Request, res: Response) => {
+    const { name, specialities, availableTime, formOfService, classStartTime, classTime, email, password } = req.body
+    if (!name || !specialities || !availableTime || !formOfService || !classStartTime || !classStartTime || !classTime || !email || !password) {
+        throw new customApiErrors.BadRequestError('Please, provide a valid payload.')
     }
+    const dateParsed = Date.parse(classStartTime)
+    const instructor = await Instructor.create({ name, specialities, availableTime, formOfService, classStartTime: dateParsed, classTime, email, password })
+
+    const token = await jwt.sign({ _id: instructor._id, name: instructor.name }, process.env.JWT_SECRET)
+
+    return res.status(StatusCodes.CREATED).json({ instructor: { _id: instructor._id, name: instructor.name }, token })
 }
 
-export const login = async (req: Request, res: Response)=>{
+export const login = async (req: Request, res: Response) => {
     res.send('Instructor login route working')
 }
 
-export const logout = async (req: Request,res: Response)=>{
+export const logout = async (req: Request, res: Response) => {
     res.send('Instructor logout route working')
 }
