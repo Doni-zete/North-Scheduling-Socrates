f46bb6cb7aad6a8d138ff11bc1aef13aec6f312d
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-08-22 18:57:22 -0300

    fix: small fix in register controller

--- src/models/instructorModel.ts
+++ src/models/instructorModel.ts
@@ -13,70 +13,62 @@ const InstructorSchema = new mongoose.Schema({
         required: [true, 'Please provide your speciality']
     }],
     availableTime: {
-       type: String,
-       required: [true, 'Please provide your available time'] 
+        type: String,
+        required: [true, 'Please provide your available time']
     },
     formOfService: {
         type: String,
         enum: ['presential', 'Online'],
         required: [true, 'Please provide your form of service']
     },
-    classStartTime:{
+    classStartTime: {
         type: Date,
         required: [true, 'Please provide the start time of the class']
     },
-    classTime:{
+    classTime: {
         type: Number,
         required: [true, 'Please provide the time of each class']
     },
     classLocation: {
         type: String
     },
-    email:{
+    email: {
         type: String,
-        required:[true, 'Please provide an email'],
-        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
+        required: [true, 'Please provide an email'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
         unique: true
     },
-    password:{
+    password: {
         type: String,
         required: [true, 'Please provide a password'],
     }
 })
 
 InstructorSchema.pre('save', async function (next) {
-    if (this.password){
+    if (this.password) {
         const salt = await bcrypt.genSalt(10)
         this.password = await bcrypt.hash(this.password, salt)
     }
     next()
 })
 
-InstructorSchema.pre('save', function(next) {
-    if (this.formOfService === 'presential') {
-        if (!this.classLocation) {
-            const error = new customApiErrors.BadRequestError('Please provide a classLocation');
-            return next(error)
-       }
-    }
-    next();
-});
+// InstructorSchema.pre('save', function(next) {
+//     if (this.formOfService === 'presential') {
+//         if (!this.classLocation) {
+//             const error = new customApiErrors.BadRequestError('Please provide a classLocation');
+//             return next(error)
+//        }
+//     }
+//     next();
+// });
 
-InstructorSchema.methods.createJWT = async function(){
-    try {
-        const jwtSecret: string = process.env.JWT_SECRET || ''
-        const token = jwt.sign(
-            {usedId: this._id, name:this.name},
-            jwtSecret,
-            {expiresIn: process.env.JWT_LIFETIME}    
-        )
-        return token
-    } catch (error) {
-        throw error
-    }
+InstructorSchema.methods.createJWT = async function () {
+    const jwtSecret = process.env.JWT_SECRET
+    return jwt.sign({ usedId: this._id, name: this.name }, jwtSecret, { expiresIn: process.env.JWT_LIFETIME }
+    )
 }
 
-InstructorSchema.methods.comparePassword = async function (candidatePassword:string) {
+InstructorSchema.methods.comparePassword = async function (candidatePassword: string) {
     return await bcrypt.compare(candidatePassword, this.password)
 }
 
