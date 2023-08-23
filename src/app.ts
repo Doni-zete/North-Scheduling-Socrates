14b696a25eff2c510a04ba2b950ee26aa377a571
doni-zete <donizete112@gmail.com>
2023-08-23 15:12:49 -0300

    feat: adding main route and student route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import 'dotenv/config'
 import 'express-async-errors'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import mongoose from 'mongoose'
 import errorHandler from './middlewares/errorHandler'
 import notFoundRoute from './middlewares/notFoundRoute'
+import studentRoute from './routes/studentRoutes'
 
 
 const app = express()
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Running')
+})
+
+app.use('/api/v1', studentRoute)
 
 app.use(notFoundRoute)
 app.use(errorHandler)
@@ -15,7 +23,7 @@ async function start() {
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log('Database connected!')
-    
+
         app.listen(process.env.PORT, () => {
             console.log(`Server running on port ${process.env.PORT}!`)
         })
