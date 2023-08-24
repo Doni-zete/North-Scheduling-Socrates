6e6d1a5afddcb406bb2e1fea07c42b45cd93f88c
doni-zete <donizete112@gmail.com>
2023-08-24 10:47:21 -0300

    feat: adding students route update and student deletion

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -5,8 +5,8 @@ const router = Router()
 
 router.post('/student/create', studentController.createStudent)
 router.get('/student/findAll', studentController.findAllStudents)
-
-
+router.put('/student/update/:id', studentController.updateStudent)
+router.delete('/student/delete/:id', studentController.deleteStudent)
 
 
 export default router
