2dfd2c09ad920e463a1c824cf9082da3da50cc8b
doni-zete <donizete112@gmail.com>
2023-08-24 10:29:45 -0300

    feat: added controller to update student

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,6 +1,8 @@
 import Student from '../models/studentModel'
 import { Request, Response } from "express"
 import { StatusCodes } from 'http-status-codes'
+import mongoose from 'mongoose';
+
 
 export const createStudent = async (req: Request, res: Response) => {
     const studentData = {
@@ -8,10 +10,33 @@ export const createStudent = async (req: Request, res: Response) => {
     };
     const students = await Student.create(studentData)
     return res.status(StatusCodes.CREATED).json(students)
-
 }
 
 export const findAllStudents = async (req: Request, res: Response) => {
     const students = await Student.find({ createdBy: req.params.studentId })
     res.status(StatusCodes.OK).json(students)
+
 }
+
+export const updateStudent = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const studentData: String = req.body
+
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(StatusCodes.BAD_REQUEST).send({ message: `Id provided is out of standard: ${id}` });
+        }
+        const updatedStudent = await Student.findByIdAndUpdate(id, studentData, { new: true });
+
+        if (!updatedStudent) {
+            return res.status(StatusCodes.NOT_FOUND).send({ message: `Student not found: ${id}` });
+        }
+        return res.status(StatusCodes.OK).json(updatedStudent);
+
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(`Unexpected error, try again ${error}`);
+    }
+}
+
+
+
