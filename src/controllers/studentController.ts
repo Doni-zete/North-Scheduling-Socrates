1dbc3915fedf2983b450aef9a7c9199f2a1cde81
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-08-25 15:55:30 -0300

    feat: added logout to studentController.

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,7 +1,7 @@
 import Student from '../models/studentModel'
 import { Request, Response } from "express"
 import { StatusCodes } from 'http-status-codes'
-import  jwt  from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import customApiErrors from '../errors/customApiErrors';
 import mongoose from 'mongoose';
 
@@ -9,7 +9,7 @@ import mongoose from 'mongoose';
 export const registerStudent = async (req: Request, res: Response) => {
     const { name, email, password, schooling } = req.body
 
-    if(!name || !email || !password || !schooling){
+    if (!name || !email || !password || !schooling) {
         throw new customApiErrors.BadRequestError('Please provide valid payload.')
     }
 
@@ -18,9 +18,9 @@ export const registerStudent = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
-    const {email, password } = req.body
+    const { email, password } = req.body
 
-    if(!email || !password) {
+    if (!email || !password) {
         throw new customApiErrors.BadRequestError('Please provide valid payload')
     }
 
@@ -32,18 +32,25 @@ export const login = async (req: Request, res: Response) => {
 
     const isPasswordCorrect = await user.comparePassword(password)
 
-    if(!isPasswordCorrect){
+    if (!isPasswordCorrect) {
         throw new customApiErrors.UnauthenticatedError('Invalid Credentials')
     }
 
-    const token = jwt.sign({id: user._id, name: user.name, role: 'student'}, process.env.JWT_SECRET)
+    const token = jwt.sign({ id: user._id, name: user.name, role: 'student' }, process.env.JWT_SECRET)
 
     res.cookie('token', token, {
         httpOnly: true,
         expires: new Date(Date.now() + 30 * 60 * 1000)
     })
 
-    res.status(StatusCodes.OK).json({user: {name: user.name}, message: 'Logged in successfully' })
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, message: 'Logged in successfully' })
+}
+
+export const logout = async (req: Request, res: Response) => {
+    res.clearCookie('token')
+    res.status(StatusCodes.OK).json({
+        msg: 'user logged out!'
+    })
 }
 
 export const findAllStudents = async (req: Request, res: Response) => {
