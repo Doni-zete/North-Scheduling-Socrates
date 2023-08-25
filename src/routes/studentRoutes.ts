b4679249947440d90535d7ce26510acd37a25338
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-08-25 16:06:08 -0300

    feat: added authenticateUser to findAllStudents, updateStudents and deleteStudent routes.

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -5,7 +5,12 @@ import authenticateUser from "../middlewares/authnetication";
 const router = Router()
 
 
-router.post('/student/create', studentController.registerStudent)
+router.post('/student/register', studentController.registerStudent)
+router.post('student/login', studentController.login)
+router.get('student/login', studentController.logout)
+
+router.use(authenticateUser)
+
 router.get('/student/findAll', studentController.findAllStudents)
 router.put('/student/update/:id', studentController.updateStudent)
 router.delete('/student/delete/:id', studentController.deleteStudent)
