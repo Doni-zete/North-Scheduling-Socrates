d892d4ea79d803d3b279d92d22793afdd9925bc9
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-08-25 15:32:36 -0300

    feat: added password hashing on StudentSchema for future use.

--- src/models/studentModel.ts
+++ src/models/studentModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 
 const StudentSchema = new mongoose.Schema({
     name: {
@@ -14,6 +15,14 @@ const StudentSchema = new mongoose.Schema({
         type: String, required: [true, 'Enter your schooling']
     },
 
+})
+
+StudentSchema.pre('save', async function(next){
+    if (this.password){
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    }
+    next()
 })
 
 const student = mongoose.model('Students', StudentSchema)
