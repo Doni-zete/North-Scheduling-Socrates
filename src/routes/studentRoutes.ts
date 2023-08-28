0915f7af9c00602017350ff1b594b1379f92ce6a
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-08-28 16:43:14 -0300

    feat: added authorizePermissions to studentRoutes.ts

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as studentController from '../controllers/studentController'
 import authenticateUser from "../middlewares/authentication";
+import { authorizePermissions } from "../middlewares/authorization";
 
 const router = Router()
 
@@ -11,9 +12,9 @@ router.get('student/login', studentController.logout)
 
 router.use(authenticateUser)
 
-router.get('/student/findAll', studentController.findAllStudents)
-router.put('/student/update/:id', studentController.updateStudent)
-router.delete('/student/delete/:id', studentController.deleteStudent)
+router.get('/student/findAll', authorizePermissions(['student']), studentController.findAllStudents)
+router.put('/student/update/:id', authorizePermissions(['student']), studentController.updateStudent)
+router.delete('/student/delete/:id', authorizePermissions(['student']), studentController.deleteStudent)
 
 
 export default router
