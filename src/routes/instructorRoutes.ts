e635f7a21b0e5dc4965c59a5f463be6b86fe7733
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-08-29 10:42:53 -0300

    fix: fix auth middleware in routers

--- src/routes/instructorRoutes.ts
+++ src/routes/instructorRoutes.ts
@@ -8,11 +8,8 @@ const instructorRouter = express.Router()
 instructorRouter.post('/instructor/register', register)
 instructorRouter.post('/instructor/login', login)
 instructorRouter.get('/instructor/logout', logout)
-
-instructorRouter.use(authenticateUser)
-
-instructorRouter.get('/instructor/findAll', authorizePermissions(['instructor']), findAllInstructors)
-instructorRouter.patch('/instructor/update/:id', authorizePermissions(['instructor']), updateInstructor)
-instructorRouter.delete('/instructor/delete/:id', authorizePermissions(['instructor']), deleteInstructor)
+instructorRouter.get('/instructor/findAll', authenticateUser, authorizePermissions(['instructor']), findAllInstructors)
+instructorRouter.patch('/instructor/update/:id', authenticateUser, authorizePermissions(['instructor']), updateInstructor)
+instructorRouter.delete('/instructor/delete/:id', authenticateUser, authorizePermissions(['instructor']), deleteInstructor)
 
 export default instructorRouter
--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -9,12 +9,9 @@ const router = Router()
 router.post('/student/register', studentController.register)
 router.post('/student/login', studentController.login)
 router.get('/student/logout', studentController.logout)
-
-router.use(authenticateUser)
-
-router.get('/student/findAll', authorizePermissions(['student']), studentController.findAllStudents)
-router.patch('/student/update/:id', authorizePermissions(['student']), studentController.updateStudent)
-router.delete('/student/delete/:id', authorizePermissions(['student']), studentController.deleteStudent)
+router.get('/student/findAll', authenticateUser, authorizePermissions(['student']), studentController.findAllStudents)
+router.patch('/student/update/:id', authenticateUser, authorizePermissions(['student']), studentController.updateStudent)
+router.delete('/student/delete/:id', authenticateUser, authorizePermissions(['student']), studentController.deleteStudent)
 
 
 export default router
