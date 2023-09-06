754eee9f9449d13648bc88914b45e0c3d0d3cb39
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-09-06 16:04:51 -0300

    refactor: setup appointmentRoutes according new controller refactor

--- src/routes/appointmentRoutes.ts
+++ src/routes/appointmentRoutes.ts
@@ -5,11 +5,11 @@ import { authorizePermissions } from '../middlewares/authorization'
 
 const Router = express.Router()
 
-Router.post('/appointment/', authenticateUser, authorizePermissions(['student', 'admin']), appointmentController.create)
+Router.post('/appointment', authenticateUser, authorizePermissions(['student', 'admin']), appointmentController.create)
 Router.get('/appointment', authenticateUser, authorizePermissions(['admin']), appointmentController.findAll)
 Router.get('/appointment/:id', authenticateUser, authorizePermissions(['admin']), appointmentController.findById)
-Router.patch('/appointment/:id', authenticateUser, authorizePermissions(['student', 'admin']), appointmentController.updateId)
-Router.delete('/appointment/:id', authenticateUser, authorizePermissions(['student', 'admin']), appointmentController.deleteId)
+Router.patch('/appointment/:id', authenticateUser, authorizePermissions(['admin']), appointmentController.updateId)
+Router.delete('/appointment/:id', authenticateUser, authorizePermissions(['admin']), appointmentController.deleteId)
 
 
 export default Router
--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -11,11 +11,11 @@ Router.post('/auth/student/register', studentController.register)
 Router.post('/auth/student/login', studentController.login)
 Router.get('/auth/student/logout', studentController.logout)
 
-Router.get('/student/', authenticateUser, authorizePermissions(['admin']), studentController.findAll)
+Router.get('/student', authenticateUser, authorizePermissions(['admin']), studentController.findAll)
 Router.get('/student/:id', authenticateUser, authorizePermissions(['student', 'admin']), studentController.findById)
-Router.get('/student/:id/appointments', authenticateUser, authorizePermissions(['student', 'admin']), studentController.findAppointmentsByStudentId)
 Router.patch('/student/:id', authenticateUser, authorizePermissions(['student', 'admin']), studentController.updateId)
 Router.delete('/student/:id', authenticateUser, authorizePermissions(['student', 'admin']), studentController.deleteId)
+Router.get('/student/:id/appointment', authenticateUser, authorizePermissions(['student', 'admin']), studentController.findAppointmentsByStudentId)
 Router.post('/student/upload', authenticateUser, authorizePermissions(['student']), uploadFile)
 
 
