e340f9a59610ddf508299140f210647f2efc8a87
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-09-06 16:01:36 -0300

    refactor: avoid some role-relationed things on appointment controller

--- src/controllers/appointmentController.ts
+++ src/controllers/appointmentController.ts
@@ -3,32 +3,24 @@ import Appointment from '../models/appointmentModel'
 import { StatusCodes } from 'http-status-codes'
 import { Request, Response } from 'express'
 import customApiErrors from '../errors/customApiErrors'
-import AppointmentDocument from '../models/appointmentDocument'
+import Availability from '../models/availabilityModel'
 
 
 const create = async (req: Request, res: Response) => {
-	const instructor = await Instructor.findById(req.body.instructorId)
-	if (!instructor) {
-		throw new customApiErrors.BadRequestError('Instructor id not found!')
+	const instructorExists = await Instructor.findById(req.body.instructorId)
+	if (!instructorExists) {
+		throw new customApiErrors.BadRequestError('Invalid instructorId, this instructor dont exists')
 	}
 
-	const availabilitySchedule = instructor.availabilitySchedule
-	for (let i = 0; i < availabilitySchedule.length; i++) {
-		const instructorCurrentDate = availabilitySchedule[i].date
-		const instructorCurrentHours = availabilitySchedule[i].availableHours
-
-		if (instructorCurrentDate === req.body.date && instructorCurrentHours.includes(req.body.hour)) {
-			const appointment = await Appointment.create(req.body)
-
-			const updatedAvailableHours = instructorCurrentHours.filter(item => item !== req.body.hour)
-			availabilitySchedule[i].availableHours = updatedAvailableHours
-			await instructor.updateOne({ availabilitySchedule })
-
-			return res.status(StatusCodes.CREATED).json({ appointment })
-		}
+	const instructorAvailability = await Availability.findOne({ instructorId: req.body.instructorId, date: req.body.date })
+	if (!instructorAvailability || !instructorAvailability.hours.includes(req.body.hour)) {
+		throw new customApiErrors.BadRequestError('Instructor is not available in this date')
 	}
 
-	throw new customApiErrors.CustomApiError('Instructor unavailable, date or hour invalid', 409)
+	instructorAvailability.hours = instructorAvailability.hours.filter(item => item !== req.body.hour)
+	await instructorAvailability.save()
+	const appointment = await Appointment.create(req.body)
+	return res.status(StatusCodes.CREATED).json({ appointment })
 }
 
 const findAll = async (req: Request, res: Response) => {
@@ -42,51 +34,26 @@ const findById = async (req: Request, res: Response) => {
 	if (!appointment) {
 		throw new customApiErrors.NotFoundError(`No item found with _id: ${req.params.id}`)
 	}
+
 	return res.status(StatusCodes.OK).json({ appointment })
 }
 
 const updateId = async (req: Request, res: Response) => {
-	let updatedAppointment: AppointmentDocument | null = null
-
-	if (req.user.role === 'student') {
-		updatedAppointment = await Appointment.findOneAndUpdate({ _id: req.params.id, studentId: req.user.id }, req.body, { new: true })
-	}
-
-	if (req.user.role === 'instructor') {
-		updatedAppointment = await Appointment.findOneAndUpdate({ _id: req.params.id, instructorId: req.user.id }, req.body, { new: true })
-	}
-
-	if ((req.user.role === 'admin')) {
-		updatedAppointment = await Appointment.findByIdAndUpdate(req.params.id, req.body, { new: true })
-	}
-
+	const updatedAppointment = await Appointment.findByIdAndUpdate(req.body.id, req.body, { new: true })
 	if (!updatedAppointment) {
-		throw new customApiErrors.NotFoundError(`No item found with _id: ${req.params.id} in yours appointments`)
+		throw new customApiErrors.NotFoundError(`No item found with _id: ${req.params.id}`)
 	}
 
 	return res.status(StatusCodes.OK).json({ updatedAppointment })
 }
 
 const deleteId = async (req: Request, res: Response) => {
-	let deletedAppointment: AppointmentDocument | null = null
-
-	if (req.user.role === 'student') {
-		deletedAppointment = await Appointment.findOneAndDelete({ _id: req.params.id, studentId: req.user.id })
-	}
-
-	if (req.user.role === 'instructor') {
-		deletedAppointment = await Appointment.findOneAndDelete({ _id: req.params.id, instructorId: req.user.id })
-	}
-
-	if ((req.user.role === 'admin')) {
-		deletedAppointment = await Appointment.findByIdAndDelete(req.params.id)
-	}
-
+	const deletedAppointment = await Appointment.findByIdAndDelete(req.body.id)
 	if (!deletedAppointment) {
-		throw new customApiErrors.NotFoundError(`No item found with _id: ${req.params.id} in yours appointments`)
+		throw new customApiErrors.NotFoundError(`No item found with _id: ${req.params.id}`)
 	}
 
-	return res.status(StatusCodes.OK).json({ deletedAppointment })
+	return res.status(StatusCodes.OK).json({ msg: 'Appointment deleted successfully' })
 }
 
 
