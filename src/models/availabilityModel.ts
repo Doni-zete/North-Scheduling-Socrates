2b44c3e296d42aeb5b235b28bcefd6ddfd0af8ce
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-09-11 15:38:42 -0300

    fix: create availability update validator in schema

--- src/models/availabilityModel.ts
+++ src/models/availabilityModel.ts
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose'
 import AvailabilityDocument from './availabilityDocument'
+import customApiErrors from '../errors/customApiErrors'
 
 
 const AvailabilitySchema = new mongoose.Schema({
 	instructorId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Instructor',
-		required: true
+		required: true,
+		immutable: true
 	},
 	date: {
 		type: String, // "2023-09-01 - AAAA-MM-DD"
 		required: [true, 'The date field is required'],
+		immutable: true,
 		match: [/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/, 'Invalid date']
 	},
 	hours: {
@@ -18,6 +21,9 @@ const AvailabilitySchema = new mongoose.Schema({
 		required: [true, 'The hours field is required'],
 		validate: {
 			validator: function (hours: string[]) {
+				if (hours.length === 0 || !hours) {
+					return false
+				}
 				const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
 				for (const hour of hours) {
 					if (!regex.test(hour)) {
@@ -25,11 +31,22 @@ const AvailabilitySchema = new mongoose.Schema({
 					}
 				}
 				return true
-			}, message: 'Invalid hour'
+			}, message: 'Invalid hours'
 		}
 	}
 })
 
+
+AvailabilitySchema.pre('findOneAndUpdate', async function (next) {
+	if (this.get('date')) {
+		throw new customApiErrors.BadRequestError('You can\'t update date field')
+	} else if (this.get('_id')) {
+		throw new customApiErrors.BadRequestError('You can\'t update _id field')
+	} else if (this.get('instructorId')) {
+		throw new customApiErrors.BadRequestError('You can\'t update instructorId field')
+	}
+	next()
+})
 
 const Availability = mongoose.model<AvailabilityDocument>('Availability', AvailabilitySchema)
 
