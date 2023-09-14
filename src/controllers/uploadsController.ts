e68716b4832c2c445b113e30fb15bcdb44666d12
sebasrib <sebastiaoleonardoribeirojunior@hotmail.com>
2023-09-14 18:16:55 -0300

    refactor: upload content controller

--- src/controllers/uploadsController.ts
+++ src/controllers/uploadsController.ts
@@ -1,65 +1,52 @@
 import { Request, Response } from 'express'
 import customApiErrors from '../errors/customApiErrors'
 import path from 'path'
-import { StatusCodes } from 'http-status-codes'
 import Appointment from '../models/appointmentModel'
+import { StatusCodes } from 'http-status-codes'
 import crypto from 'crypto'
 import fs from 'fs'
 
+
 const uploadFilebyAppointmentId = async (req: Request, res: Response) => {
-
-	const files = req.files
-
-	if (!files) {
-		throw new customApiErrors.BadRequestError('Nenhum arquivo foi enviado')
+	if (!req.files || Object.keys(req.files).length === 0) {
+		throw new customApiErrors.BadRequestError('No files were uploaded')
 	}
 
-	const tmpPath = path.join(__dirname, '../tmp')
+	if (req.files.foo instanceof Array) {
+		throw new customApiErrors.BadRequestError('You can not upload more than one file per time')
+	}
 
+	const allowedExtension = ['.pdf', '.txt', '.docx']
+	
+	let fileExtension = path.extname(req.files.foo.name)
+	if (fileExtension === '.ocx') {
+		fileExtension = '.docx'
+	}
+	
+	if (!allowedExtension.includes(fileExtension)) {
+		throw new customApiErrors.BadRequestError('Apenas arquivos PDF, TXT e Word são permitidos')
+	}
+	
+	const appointment = await Appointment.findById(req.params.id)
+	if (!appointment) {
+		throw new customApiErrors.NotFoundError('Appointment not found')
+	}
+	
+	if (appointment.studentId.toString() !== req.user.id) {
+		throw new customApiErrors.UnauthorizedError('You can not attach in appointment of others!')
+	}
+	
+	const tmpPath = path.join(__dirname, '../tmp')
 	if (!fs.existsSync(tmpPath)) {
 		fs.mkdirSync(tmpPath)
 	}
 
-	const allowedExtension = ['.pdf', '.txt', '.ocx']
+	const uniqueFileName = crypto.randomUUID() + fileExtension
+	const urlPath = `/tmp/${uniqueFileName}`
 
-	const UploadedFiles = []
-
-	for (const fileKey in files) {
-		if (Object.hasOwnProperty.call(files, fileKey)) {
-			const file = files[fileKey]
-			let fileExtension = path.extname(file.name).toLowerCase()
-			if (!allowedExtension.includes(fileExtension)) {
-				throw new customApiErrors.BadRequestError('Apenas arquivos PDF, TXT e Word são permitidos')
-			}
-
-			const appointmentId = req.params.id
-			const appointment = await Appointment.findById(appointmentId)
-
-			if (!appointment) {
-				throw new customApiErrors.NotFoundError('No Appointment with this id')
-			}
-
-			if (req.user.role === 'student' && req.params.studentId !== req.user.id) {
-				throw new customApiErrors.UnauthorizedError('You can not update a appointment of others students!')
-			} else if (req.user.role === 'instructor' && appointment.instructorId.toString() !== req.user.id) {
-				// throw new error
-			}
-
-			if (fileExtension === '.ocx') {
-				fileExtension = '.docx'
-			}
-
-			const uniqueFileName = crypto.randomUUID() + fileExtension
-			const uploadPath = path.join(__dirname, '../tmp/', uniqueFileName)
-			await file.mv(uploadPath)
-			UploadedFiles.push(uploadPath)
-
-			appointment.attachments.push(`/tmp/${uniqueFileName}`)
-			await appointment.save()
-
-			res.status(StatusCodes.OK).json({ file: { src: `/tmp/${uniqueFileName}` } })
-		}
-	}
+	appointment.attachments.push(urlPath)
+	appointment.save()
+	res.status(StatusCodes.CREATED).json({ urlPath })
 }
 
 const uploadController = { uploadFilebyAppointmentId }
