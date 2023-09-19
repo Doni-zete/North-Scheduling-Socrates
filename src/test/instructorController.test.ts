1fa54364967c6033e1e0ee47a9320c4bef5d51d5
doni-zete <donizete112@gmail.com>
2023-09-19 10:47:05 -0300

    feat: Creating login test for instructor

--- src/test/instructorController.test.ts
+++ src/test/instructorController.test.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import instructorController from '../controllers/instructorController'
 import Instructor from '../models/instructorModel'
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 jest.mock('../models/instructorModel', () => ({
 	create: jest.fn(),
@@ -37,4 +38,50 @@ describe('Instructor Registration', () => {
 	})
 })
 
+
+describe('Instructor Login', () => {
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	it('should successfully login an instructor', async () => {
+		const req = {
+			body: {
+				email: 'maria@email.com',
+				password: 'password123',
+			},
+		} as Request
+
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+			cookie: jest.fn(),
+		} as unknown as Response
+
+		const mockedInstructor = {
+			_id: 'mockedId',
+			name: 'Maria',
+			role: 'instructor',
+			comparePassword: jest.fn().mockResolvedValue(true),
+		}
+
+			; (Instructor.findOne as jest.Mock).mockResolvedValue(mockedInstructor)
+
+		await instructorController.login(req, res)
+
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
+		expect(res.json).toBeCalledWith({
+			instructor: {
+				_id: mockedInstructor._id,
+				name: mockedInstructor.name,
+				role: mockedInstructor.role,
+			},
+			msg: 'Logged in successfully',
+		})
+
+		expect(Instructor.findOne).toHaveBeenCalledWith({ email: req.body.email })
+	})
+
+
+})
 
