cf68e5a2336f55f0e56f824839d7c5eccf4d84f4
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-09-20 11:43:15 -0300

    feat: Students findAll test created

--- src/test/studentController.test.ts
+++ src/test/studentController.test.ts
@@ -9,6 +9,7 @@ jest.mock('../models/studentModel', () => ({
 	create: jest.fn(),
 	findOne: jest.fn(),
 	comparePassword: jest.fn(),
+	find: jest.fn(),
 	findById: jest.fn(),
 }))
 
@@ -81,5 +82,38 @@ describe('Students Login', () => {
 
 		expect(Student.findOne).toHaveBeenCalledWith({ email: req.body.email })
 
+	})
+})
+
+describe('Should list all students', () => {
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	it('should successfully get a list of students', async () => {
+		const req = {} as Request
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		} as unknown as Response
+
+		const mockedStudents = [
+			{_id: 'studentId1', name: 'Student 1', role: 'student'},
+			{_id: 'studentId2', name: 'Student 2', role: 'student'}
+		];
+    
+		(Student.find as jest.Mock).mockReturnValue({
+			select: jest.fn().mockResolvedValue(mockedStudents)
+		})
+    
+		await studentController.findAll(req,res)
+
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
+		expect(res.json).toHaveBeenCalledWith({
+			students: mockedStudents
+		})
+		expect(Student.find).toHaveBeenCalledWith({})
+
+		expect((Student.find as jest.Mock).mock.results[0].value.select).toHaveBeenCalledWith('-password')
 	})
 })
