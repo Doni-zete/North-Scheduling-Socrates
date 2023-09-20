91e5a2db5571f5c22a6ca653d795d916fadd8052
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-09-20 13:49:39 -0300

    feat: Students findById created

--- src/test/studentController.test.ts
+++ src/test/studentController.test.ts
@@ -117,3 +117,41 @@ describe('Should list all students', () => {
 		expect((Student.find as jest.Mock).mock.results[0].value.select).toHaveBeenCalledWith('-password')
 	})
 })
+
+describe('Should find a student by ID', () => {
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	it('should successfully find a student by ID for admin', async () => {
+		const req: Request = {
+			user: { role: 'admin', id: 'adminId' },
+			params: { id: 'studentId' }
+		} as unknown as Request
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		} as unknown as Response
+
+		const mockedStudent = {
+			_id: 'studentId',
+			name: 'Student 1',
+			role: 'student'
+		};
+
+		(Student.findById as jest.Mock).mockReturnValue({
+			select: jest.fn().mockResolvedValue(mockedStudent)
+		})
+
+		await studentController.findById(req,res)
+
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
+		expect(res.json).toHaveBeenCalledWith({
+			student: mockedStudent
+		})
+
+		expect(Student.findById).toHaveBeenCalledWith('studentId')
+
+		expect((Student.findById as jest.Mock).mock.results[0].value.select).toHaveBeenCalledWith('-password')
+	})
+})
