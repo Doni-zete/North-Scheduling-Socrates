5e0093a310a08125fe704cfcc176a3eda7be5e29
doni-zete <donizete112@gmail.com>
2023-09-20 21:08:08 -0300

    fix: format tab in test

--- src/test/instructorTest/instructorFindAll.test.ts
+++ src/test/instructorTest/instructorFindAll.test.ts
@@ -5,34 +5,34 @@ import Instructor from '../../models/instructorModel'
 
 
 describe('Instructors findAll', () => {
-    it('should return all instructors', async () => {
-        const req = {} as Request
-        const res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-        } as any as Response;
+	it('should return all instructors', async () => {
+		const req = {} as Request
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		} as any as Response;
 
-        const mockInstructors = [
-            {
-                _id: '64f36a6ada776022dcf594ba',
-                name: 'Azul',
-                email: 'azul@gmail.com',
-                password: '123',
-            },
-            {
-                _id: '64f36cc5da776022dcf595f8',
-                name: 'Laranja',
-                email: 'laranja@email.com',
-                password: '123',
-            },
-        ]
+		const mockInstructors = [
+			{
+				_id: '64f36a6ada776022dcf594ba',
+				name: 'Azul',
+				email: 'azul@gmail.com',
+				password: '123',
+			},
+			{
+				_id: '64f36cc5da776022dcf595f8',
+				name: 'Laranja',
+				email: 'laranja@email.com',
+				password: '123',
+			},
+		]
 
-        const selectMock = jest.fn().mockReturnValue(mockInstructors)
-        Instructor.find = jest.fn().mockReturnValue({ select: selectMock })
+		const selectMock = jest.fn().mockReturnValue(mockInstructors)
+		Instructor.find = jest.fn().mockReturnValue({ select: selectMock })
 
-        await instructorController.findAll(req, res)
+		await instructorController.findAll(req, res)
 
-        expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
-        expect(res.json).toHaveBeenCalledWith({ instructors: mockInstructors })
-    })
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
+		expect(res.json).toHaveBeenCalledWith({ instructors: mockInstructors })
+	})
 })
--- src/test/instructorTest/instructorUdate.test.ts
+++ src/test/instructorTest/instructorUdate.test.ts
@@ -1,45 +1,44 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import instructorController from '../../controllers/instructorController';
-import Instructor from '../../models/instructorModel';
-import customApiErrors from '../../errors/customApiErrors';
-
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import instructorController from '../../controllers/instructorController'
+import Instructor from '../../models/instructorModel'
+import customApiErrors from '../../errors/customApiErrors'
 
 describe('Instructors Update', () => {
-    it('should return an instructor updated by id', async () => {
-        const req = {
-            body: {
-                oldPassword: '123',
-                password: '1234',
-            },
-        } as any as Request
+	it('should return an instructor updated by id', async () => {
+		const req = {
+			body: {
+				oldPassword: '123',
+				password: '1234',
+			},
+		} as any as Request
 
-        const jsonMock = jest.fn()
-        const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
+		const jsonMock = jest.fn()
+		const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
 
-        const res: Response = {
-            status: statusMock,
-        } as any as Response
+		const res: Response = {
+			status: statusMock,
+		} as any as Response
 
-        const mockInstructor = {
-            _id: '6508854a72a40e21e3c62efd',
-            oldPassword: "123",
-            password: "1234",
+		const mockInstructor = {
+			_id: '6508854a72a40e21e3c62efd',
+			oldPassword: '123',
+			password: '1234',
+		}
 
-        }
+		Instructor.findOne = jest.fn().mockResolvedValue(mockInstructor)
 
-        Instructor.findOne = jest.fn().mockResolvedValue(mockInstructor);
+		try {
+			const updatedInstructor = await instructorController.updateId(req, res)
 
-        try {
-            const updatedInstructor = await instructorController.updateId(req, res);
+			if (!updatedInstructor) {
+				throw new customApiErrors.NotFoundError(
+					`No item found with _id: ${req.params.id}`
+				)
+			}
 
-            if (!updatedInstructor) {
-                throw new customApiErrors.NotFoundError(`No item found with _id: ${req.params.id}`);
-            }
-
-            expect(statusMock).toHaveBeenCalledWith(StatusCodes.OK);
-            expect(jsonMock).toHaveBeenCalledWith({ updatedInstructor: mockInstructor });
-        } catch (error) {
-        }
-    })
+			expect(statusMock).toHaveBeenCalledWith(StatusCodes.OK)
+			expect(jsonMock).toHaveBeenCalledWith({ updatedInstructor: mockInstructor })
+		} catch (error) { }
+	})
 })
