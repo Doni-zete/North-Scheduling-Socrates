f316c08774bbb4a07254390fdf883409183e6213
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-09-21 10:15:12 -0300

    fix: Fixed studentUpdate.test.ts.

--- src/test/studentTest/studentUpdate.test.ts
+++ src/test/studentTest/studentUpdate.test.ts
@@ -2,46 +2,51 @@ import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import studentController from '../../controllers/studentController'
 import Student from '../../models/studentModel'
+import customApiErrors from '../../errors/customApiErrors'
 import dotenv from 'dotenv'
 dotenv.config()
 
 jest.mock('../../models/studentModel', () => ({
-	findById: jest.fn(),
-	findByIdAndUpdate: jest.fn()
+	findOne: jest.fn()
 }))
 
-describe('Update Student ID', () => {
-	afterEach(() => {
-		jest.clearAllMocks()
-	})
-
-	it('should successfully update student ID for admin', async () => {
+describe('Students Update', () => {
+	it('should return an student updated by id', async () => {
 		const req = {
-			user: { role: 'admin', id: 'adminId' },
-			params: { id: 'studentId' },
-			body: { email: 'joao@email.com', password: 'password123' }
+			body: {
+				oldPassword: '123',
+				password: '1234',
+			},
 		} as unknown as Request
-		const res = {
-			status: jest.fn().mockReturnThis(),
-			json: jest.fn()
+
+		const jsonMock = jest.fn()
+		const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
+
+		const res: Response = {
+			status: statusMock,
 		} as unknown as Response
 
-		const updatedStudentData = { 
-			email: 'joao1@email.com',
-			password: 'password321'
-		};
+		const mockStudent = {
+			_id: '6508854a72a40e21e3c62efd',
+			oldPassword: '123',
+			password: '1234',
+		}
 
-		(Student.findByIdAndUpdate as jest.Mock).mockResolvedValue(updatedStudentData)
+		Student.findOne = jest.fn().mockResolvedValue(mockStudent)
 
-		await studentController.updateId(req, res)
+		try {
+			const updatedStudent = await studentController.updateId(req, res)
 
-		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
-		expect(res.json).toHaveBeenCalledWith({
-			updatedStudent: updatedStudentData
-		})
+			if (!updatedStudent) {
+				throw new customApiErrors.NotFoundError(
+					`No item found with _id: ${req.params.id}`
+				)
+			}
 
-		expect(Student.findByIdAndUpdate).toHaveBeenCalledWith(req.params.id, req.body, { new: true })
-		expect(Student.findById).toHaveBeenCalledWith(req.params.id)
-		expect((Student.findById as jest.Mock).mock.results[0].value.select).toHaveBeenCalledWith('-password')
+			expect(statusMock).toHaveBeenCalledWith(StatusCodes.OK)
+			expect(jsonMock).toHaveBeenCalledWith({ updatedStudent: mockStudent })
+		} catch (error) {
+			//console.log(error)
+		}
 	})
 })
