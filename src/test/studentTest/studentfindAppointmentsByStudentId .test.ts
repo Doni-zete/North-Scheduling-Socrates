e4440966f98375209a73d96546c795b85c81e52a
doni-zete <donizete112@gmail.com>
2023-09-22 11:01:54 -0300

    refactor:removendo try catch

--- src/test/studentTest/studentfindAppointmentsByStudentId .test.ts
+++ src/test/studentTest/studentfindAppointmentsByStudentId .test.ts
@@ -1,57 +1,44 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import studentController from '../../controllers/studentController'
-import Student from '../../models/studentModel'
-import customApiErrors from '../../errors/customApiErrors'
+import Appointment from '../../models/appointmentModel'
 
-jest.mock('../../models/studentModel', () => ({
-	findAppointmentsByStudentId: jest.fn(),
+jest.mock('../../models/appointmentModel', () => ({
 	find: jest.fn()
-
 }))
 
 describe('Should findAppointmentsByStudentId', () => {
 
 	it('Should find appointment student by ID', async () => {
-		const req: Request = {
-			params: { id: '65044adc60353efe5a88c2a3' },
+		const req = {
+			user: { role: 'admin', id: '6500e1b902055bfcc7527f00' },
+			params: { id: '6500e1b902055bfcc7527f00' },
+	
 		} as unknown as Request
 
+		const jsonMock = jest.fn()
+		const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
+
 		const res = {
-			status: jest.fn().mockReturnThis(),
-			json: jest.fn(),
+			status: statusMock,
 		} as unknown as Response
 
 		const mockAppoitments = {
 			name: 'Blue',
 			instructorId: '65044bf360353efe5a88c2ab',
-			student: '65044adc60353efe5a88c2a3',
+			student: '6500e1b902055bfcc7527f00',
 			date: '2023-09-05',
 			hour: '10:00',
 			attachments: '/tmp/2ab31c01-150a-4d3b-a1a6-62d71102cac6.txt',
-		}
+		};
 
+		(Appointment.find as jest.Mock).mockReturnValue({
+			select: jest.fn().mockResolvedValue(mockAppoitments)
+		})
+		await studentController.findAppointmentsByStudentId(req, res)
 
-		try {
-			const updatedStudent = await studentController.findAppointmentsByStudentId(req, res)
+		expect(statusMock).toHaveBeenCalledWith(StatusCodes.OK)
+		expect(jsonMock).toHaveBeenCalledWith({appointment: mockAppoitments})
 
-			if (!updatedStudent) {
-				throw new customApiErrors.NotFoundError(
-					`No item found with _id: ${req.params.id}`
-				)
-			}
-
-			Student.find = jest.fn().mockResolvedValue(mockAppoitments)
-
-			expect(res.status).toHaveBeenCalledWith(StatusCodes.OK)
-			expect(res.json).toHaveBeenCalledWith({
-				appointment: mockAppoitments
-			})
-			console.log(updatedStudent)
-
-		} catch (error) {
-			//console.log(error)
-		}
 	})
-
 })
